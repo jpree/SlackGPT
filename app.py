8d3daac805b01ee8f0e7e70c5cfe511d4a674279
jpreece <5642034+jpree@users.noreply.github.com>
2023-05-06 15:29:41 -0400

    Added conversation history

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
-import sys
 import logging
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from argparse import ArgumentParser
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.schema import AIMessage, HumanMessage
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
@@ -17,62 +15,60 @@ logger = logging.getLogger(__name__)
 def initialize_app():
     return App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-# Initialize the Slack app
 app = initialize_app()
 
-# Initialize the conversational retrieval chain
-def initialize_chain():
-    embeddings = OpenAIEmbeddings()
-    vec_search = Chroma(embedding_function=embeddings, persist_directory=".coupa")
-
-    return RetrievalQAWithSourcesChain.from_chain_type(
-        ChatOpenAI(
-            temperature=0.9,
-            model_name="gpt-3.5-turbo",#"gpt-4",#gpt-3.5-turbo",
-            verbose=True
-        ),
-        chain_type="stuff",
-        retriever=vec_search.as_retriever(k=5)
+def initialize_chat_model():
+    return ChatOpenAI(
+        temperature=0.9,
+        model_name="gpt-3.5-turbo",
+        verbose=True
     )
 
-# Process a message using the conversational retrieval chain
-def process_message(chain, message_text):
+chat_model = initialize_chat_model()
+
+def get_chat_history(channel_id, bot_user_id):
+    response = app.client.conversations_history(channel=channel_id, limit=10)
+
+    messages = []
+    for msg in response["messages"]:
+        if msg['user'] == bot_user_id:
+            messages.insert(0, AIMessage(content=msg['text']))
+        else:
+            messages.insert(0, HumanMessage(content=msg['text']))
+
+    return messages
+
+def get_bot_user_id():
     try:
-        resp = chain({"question":message_text}, return_only_outputs=True)
-        return resp["answer"]
+        bot_user_id = app.client.auth_test()["user_id"]
+        return bot_user_id
+    except Exception as e:
+        logger.error(f"Error retrieving bot user info: {e}")
+        return None
+
+def process_message(message_text, channel_id, bot_user_id):
+    try:
+        logger.debug(f"process_message: {message_text} {channel_id} {bot_user_id}")
+        chat_history = get_chat_history(channel_id, bot_user_id)
+        response = chat_model(chat_history)
+        return response.content
     except Exception as e:
         logger.error(f"Error processing message: {e}")
         return "Sorry, something went wrong. Please try again later."
 
-# Handle incoming messages that mention the bot
 @app.message(".*")
 def handle_mention(message, say, logger):
-    # Validate the message
-    if not message or "text" not in message or not message["text"]:
-        logger.warning("Invalid or empty message received")
-        return
+    logger.info("handle_mention")
+    chat_response = process_message(message["text"], message["channel"], get_bot_user_id())
+    say(chat_response)
 
-    # Log the message at DEBUG level
-    logger.debug(message)
-    message_text = message["text"]
-    response = process_message(chain, message_text)
-    say(response)
-
-# Handle app_mention events
 @app.event("app_mention")
 def handle_app_mention_events(body, say, logger):
-    # Validate the event
-    if not body or "event" not in body or "text" not in body["event"] or not body["event"]["text"]:
-        logger.warning("Invalid or empty event received")
-        return
-
-    # Log the event at DEBUG level
-    logger.debug(body)
+    logger.debug("handle_app_mention_events")
     message_text = body["event"]["text"]
-    response = process_message(chain, message_text)
-    say(response)
+    channel_id = body["event"]["channel"]
+    chat_response = process_message(message_text, channel_id, get_bot_user_id())
+    say(chat_response)
 
-# Main entry point
 if __name__ == "__main__":
-    chain = initialize_chain()
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
