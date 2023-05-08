d767ab015622f8985ace3d9b74259f508235e243
jpreece <5642034+jpree@users.noreply.github.com>
2023-05-07 21:59:23 -0400

    added language model override

--- chat_module/config.py
+++ chat_module/config.py
@@ -5,9 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PERSONA_TOKEN = ";persona;"
-INCLUDED_CHAT_HISTORY = 1
+DEFAULT_CHAT_HISTORY = 10
+DEFAULT_LANGUAGE_MODEL = "gpt-3.5-turbo"
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
+INCLUDED_CHAT_HISTORY = os.environ.get("INCLUDED_CHAT_HISTORY")
+LANGUAGE_MODEL = os.environ.get("LANGUAGE_MODEL")
+
+if INCLUDED_CHAT_HISTORY is not None:
+    INCLUDED_CHAT_HISTORY = int(INCLUDED_CHAT_HISTORY)
+else:
+    INCLUDED_CHAT_HISTORY = DEFAULT_CHAT_HISTORY
+
+if LANGUAGE_MODEL is not None:
+    LANGUAGE_MODEL = str(LANGUAGE_MODEL)
+else:
+    LANGUAGE_MODEL = DEFAULT_LANGUAGE_MODEL
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
